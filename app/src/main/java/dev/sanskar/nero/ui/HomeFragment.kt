4076acdbac5875453dc949d7c5a09da15487c97b
Sanskar Agrawal <sanskar10100@gmail.com>
2022-09-20 14:55:24 +0530

    Add starter code, dependencies and app icon

---
+++ app/src/main/java/dev/sanskar/nero/ui/HomeFragment.kt
@@ -0,0 +1,31 @@
+package dev.sanskar.nero.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                HomeScreen()
+            }
+        }
+    }
+
+    @Composable
+    fun HomeScreen(modifier: Modifier = Modifier) {
+
+    }
+}
